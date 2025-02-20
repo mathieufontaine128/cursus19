d86c2a99825e245fb48807756e41238ee54790c9
Mathieu Fontaine <mfontain@shi-r7-p3.s19.be>
2025-02-20 13:00:16 +0100

    fin de journee

---
+++ C07/ex02/ft_ultimate_range.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfontain <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 12:56:31 by mfontain          #+#    #+#             */
+/*   Updated: 2025/02/20 12:59:13 by mfontain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	len;
+
+	len = max - min;
+
+}
+int main()
+{
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+/*{
+	
+	int len;
+	int **tab;
+	int i;
+
+	if (min >= max)
+		return (NULL);
+	len = max -min;
+	i = 0;
+	*tab = (int *)malloc (len * sizeof(int));
+	if (tab* == NULL)
+		return (-1);
+	while (i < )
+	{
+		tab*[i] = min + i;
+		i++;
+	}
+	return(len);
+}
+#include <stdio.h>
+int main()
+{
+	int *array;
+	int size = ft_ultimate_range(array, 1, 8); 
+	
+}*/
